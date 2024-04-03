9b90e402d73321ff24d9fde13ccf41f7b662d72f
Yorick Yoranda <yorandayorick09@gmail.com>
2024-04-03 13:55:01 +0700

    first commit

---
+++ app/src/main/java/com/yorickyoranda/webviewlanjutan/MainActivity.java
@@ -0,0 +1,38 @@
+package com.yorickyoranda.webviewlanjutan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView _webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _webView = (WebView) findViewById(R.id.webView1);
+
+        WebViewClient webViewClient =  new WebViewClient();
+        _webView.setWebViewClient(webViewClient);
+
+        WebChromeClient webChromeClient = new WebChromeClient();
+        _webView.setWebChromeClient(webChromeClient);
+
+        WebSettings webSettings = _webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        WebAppInterface webAppInterface = new WebAppInterface(this, MainActivity.this);
+        _webView.addJavascriptInterface(webAppInterface, "Android");
+
+        String url = "https://yorick-project.000webhostapp.com/212102384/webview-lanjutan.html";
+        _webView.loadUrl(url);
+    }
+}
